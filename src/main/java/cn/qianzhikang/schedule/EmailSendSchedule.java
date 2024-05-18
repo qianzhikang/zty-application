f96ba6f9d03e511670dd1d9956b1567fb67d0834
lumingxia <lmx@e-plant.cn>
2024-05-18 15:52:55 +0800

    定时任务的具体代码

---
+++ src/main/java/cn/qianzhikang/schedule/EmailSendSchedule.java
@@ -0,0 +1,13 @@
+package cn.qianzhikang.schedule;
+
+import cn.hutool.cron.CronUtil;
+
+/**
+ * 邮件发送定时任务
+ */
+public class EmailSendSchedule {
+    public void sendEmailSchedule()
+    {
+        System.out.println("邮件任务");
+    }
+}
