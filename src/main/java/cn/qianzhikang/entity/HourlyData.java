a00f18b330cd03df02f2f1e6e7ad8d938677dc00
qianzhikang <qzkqianzhikang@gmail.com>
2024-05-18 15:02:08 +0800

    添加注释

--- src/main/java/cn/qianzhikang/entity/HourlyData.java
+++ src/main/java/cn/qianzhikang/entity/HourlyData.java
@@ -7,18 +7,32 @@ import lombok.Data;
  */
 @Data
 public class HourlyData {
+    /** 预报时间*/
     private String fxTime;
+    /** 温度*/
     private String temp;
+    /** 天气状况的图标代码*/
     private String icon;
+    /** 天气状况的文字描述*/
     private String text;
+    /** 风向360角度*/
     private String wind360;
+    /** 风向*/
     private String windDir;
+    /** 风力等级*/
     private String windScale;
+    /** 风速，公里/小时*/
     private String windSpeed;
+    /** 相对湿度，百分比数值*/
     private String humidity;
+    /** 当前小时累计降水量，默认单位：毫米*/
     private String pop;
+    /** 逐小时预报降水概率，百分比数值*/
     private String precip;
+    /** 大气压强，默认单位：百帕*/
     private String pressure;
+    /** 云量，百分比数值。可能为空*/
     private String cloud;
+    /** 露点温度。可能为空*/
     private String dew;
 }
--- src/main/java/cn/qianzhikang/entity/WeatherHourlyData.java
+++ src/main/java/cn/qianzhikang/entity/WeatherHourlyData.java
@@ -9,9 +9,14 @@ import java.util.List;
  */
 @Data
 public class WeatherHourlyData {
+    /** 状态码 */
     private String code;
+    /** 当前API的最近更新时间 */
     private String updateTime;
+    /** 当前数据的响应式页面，便于嵌入网站或应用*/
     private String fxLink;
+    /** 小时数据 */
     private List<HourlyData> hourly;
+    /** 原始数据来源 */
     private Refer refer;
 }
